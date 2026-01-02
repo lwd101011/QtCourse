a9ac7d9b32991b849b2d89bb15c5cf21e8096848
lwd101011 <466712445@qq.com>
2026-01-02 19:03:10 +0800

    实现基本的槽函数原型及辅助调试功能

--- lab4/lab4/ChatClient/mainwindow.cpp
+++ lab4/lab4/ChatClient/mainwindow.cpp
@@ -6,9 +6,29 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentWidget(ui->loginPage);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_loginButton_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->chatPage);
+}
+
+void MainWindow::on_sayButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_logoutButton_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->loginPage);
+}
+
+
+
--- lab4/lab4/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/ChatServer/debug/moc_mainwindow.cpp
+++ lab4/lab4/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/ChatServer/debug/moc_mainwindow.cpp
@@ -38,10 +38,20 @@ template <> constexpr inline auto MainWindow::qt_create_metaobjectdata<qt_meta_t
 {
     namespace QMC = QtMocConstants;
     QtMocHelpers::StringRefStorage qt_stringData {
-        "MainWindow"
+        "MainWindow",
+        "on_startStopButton_clicked",
+        "",
+        "logMessage",
+        "msg"
     };
 
     QtMocHelpers::UintData qt_methods {
+        // Slot 'on_startStopButton_clicked'
+        QtMocHelpers::SlotData<void()>(1, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'logMessage'
+        QtMocHelpers::SlotData<void(const QString &)>(3, 2, QMC::AccessPublic, QMetaType::Void, {{
+            { QMetaType::QString, 4 },
+        }}),
     };
     QtMocHelpers::UintData qt_properties {
     };
@@ -63,10 +73,13 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     auto *_t = static_cast<MainWindow *>(_o);
-    (void)_t;
-    (void)_c;
-    (void)_id;
-    (void)_a;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->on_startStopButton_clicked(); break;
+        case 1: _t->logMessage((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        default: ;
+        }
+    }
 }
 
 const QMetaObject *MainWindow::metaObject() const
@@ -85,6 +98,18 @@ void *MainWindow::qt_metacast(const char *_clname)
 int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 2;
+    }
     return _id;
 }
 QT_WARNING_POP
